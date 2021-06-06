3ee729be87166fb5f4fc74c1b5706e44762c1a3e
dlarocque <daniellrq@outlook.com>
2021-06-06 14:21:18 -0500

    Added sticky nav bar

--- src/components/navigation/NavigationBar.vue
+++ src/components/navigation/NavigationBar.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="navigationbar">
-    <v-app-bar dark>
-      <!-- v-app-bar-title text is truncated in 2.4.0, using span instead -->
-      <span class="appbartitle">Daniel La Rocque</span>
-      <v-spacer></v-spacer>
-      <nav>
-        <router-link to="/" class="routerlink">Home</router-link>
-        <router-link to="/about" class="routerlink">About</router-link>
-        <router-link to="/projects" class="routerlink">Projects</router-link>
-        <router-link to="/contact" class="routerlink">Contact</router-link>
-      </nav>
-    </v-app-bar>
-  </div>
+  <v-app-bar dark fixed app>
+    <!-- v-app-bar-title text is truncated in 2.4.0, using span instead -->
+    <span class="appbartitle">Daniel La Rocque</span>
+    <v-spacer></v-spacer>
+    <nav>
+      <router-link to="/" class="routerlink">Home</router-link>
+      <router-link to="/about" class="routerlink">About</router-link>
+      <router-link to="/projects" class="routerlink">Projects</router-link>
+      <router-link to="/contact" class="routerlink">Contact</router-link>
+    </nav>
+  </v-app-bar>
 </template>
 
 <script>
@@ -24,10 +22,6 @@ export default {
 <style scoped>
 .appbartitle {
   font-size: 20px;
-}
-
-.v-app-bar-title {
-  color: whitesmoke;
 }
 
 .routerlink {
