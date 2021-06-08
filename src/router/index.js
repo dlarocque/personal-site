dd069cb4250c6358c229cf6780ef98a4da99e439
dlarocque <daniellrq@outlook.com>
2021-06-08 09:44:02 -0500

    Fixed double home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-import Contact from "../views/Contact.vue";
-import Projects from "../views/Projects.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
-  {
-    path: "/contact",
-    name: "Contact",
-    component: Contact,
-  },
-  {
-    path: "/projects",
-    name: "Projects",
-    component: Projects,
-  },
-];
+const routes = [];
 
 const router = new VueRouter({
   routes,
