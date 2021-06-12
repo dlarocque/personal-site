f06e06a55c49c4856d9757876ebfeafe55bd0289
dlarocque <daniellrq@outlook.com>
2021-06-11 20:06:52 -0500

    Finished about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,22 +5,12 @@
       Hey! I'm Daniel, a third year computer science undergraduate student at
       the University of Manitoba.<br /><br />
       Since I'm still only 19 years old, I don't really have much to say for
-      myself! I grew up in
-      <a
-        href="https://static.wikia.nocookie.net/simpsons/images/0/0e/Winnipeg.jpg/revision/latest?cb=20120719011109"
-        target="blank"
-        >Winnipeg</a
-      >
-      , spent my time playing hockey and video games, while trying not to die
-      from the
-      <a
-        href="https://www.cbc.ca/news/canada/manitoba/winnipeg-deep-freeze-as-cold-as-uninhabited-planet-1.2479967"
-        target="blank"
-        >brutal climate</a
-      >. I fell into my passion of Computer Science in my first year of
-      University, in my university's intro CS course. After spending two years
-      learning the ropes in University, I'm now learning about the practical
-      applications of my studies through my first summer internship at
+      myself! I grew up in Winnipeg , spent my time playing hockey and video
+      games, while trying not to die from the brutal climate. I fell into my
+      passion of Computer Science in my first year of University, in my
+      university's intro CS course. After spending two years learning the ropes
+      in University, I'm now learning about the practical applications of my
+      studies through my first summer internship at
       <a href="https://www.bell.ca/Mobility/Our_network" target="blank"
         >Bell Canada</a
       >
@@ -29,7 +19,7 @@
       <router-link :to="{ path: '/', hash: '#projects' }" class="routerlink"
         >personal projects</router-link
       >, or whatever I find interesting, really.<br /><br />
-      If, for whatever reason, you're interested in knowing more about me,
+      If for whatever reason, you're interested in knowing more about me,
       <router-link :to="{ path: '/', hash: '#contact' }" class="routerlink"
         >contact me!</router-link
       >
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,10 @@
     <h1>Daniel La Rocque</h1>
     <h3>Undergraduate Computer Science Student</h3>
     <p>
-      This is where you can some basic information about me, but mainly the
-      projects I've worked on. The projects I have are still limited, but I hope
-      that one day this site will be full of interesting and diverse projects.
+      This is where you can find all of the projects I've worked on as well as
+      some information about me! The projects I have are still limited, but I
+      hope that one day this site will be full of interesting and diverse
+      projects.
     </p>
   </section>
 </template>
